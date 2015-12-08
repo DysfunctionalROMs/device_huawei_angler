3e808a1851d8c95f42107c67fdc0536d64508eed
Susan McCombs <smac0628@gmail.com>
2015-12-07 22:34:16 -0500

    thermal: relax throttle
    Also add two additional threads for dexpreopt.
    This will speed up app installs as well as
    initial boot.
    
    Change-Id: I40f30e0621906d4256fb696540d0f02d656f6f9f

--- device.mk
+++ device.mk
@@ -368,9 +368,8 @@ PRODUCT_PACKAGES += \
 
 # limit dex2oat threads to improve thermals
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.boot-dex2oat-threads=4 \
-    dalvik.vm.dex2oat-threads=2 \
-    dalvik.vm.image-dex2oat-threads=4
+    dalvik.vm.dex2oat-threads=4 \
+    dalvik.vm.image-dex2oat-threads=6
 
 # old-apns.conf
 PRODUCT_COPY_FILES += \
