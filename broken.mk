eab26eedddf9d0b587760b80a40ed9517d35dfc6
drgroovestarr <jarrod.worlitz@gmail.com>
2015-12-27 17:58:44 -0800

    Broke-it

---
+++ broken.mk
@@ -0,0 +1,26 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+# Inherit Broken common.
+$(call inherit-product, vendor/broken/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/broken/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/huawei/angler/aosp_angler.mk)
+
+#DEVICE_PACKAGE_OVERLAYS += device/huawei/angler/overlay-cm
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := broken_angler
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 6P
+
+TARGET_VENDOR := huawei
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=angler \
+    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MMB29M/2431559:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 6.0.1 MMB29M 2431559 release-keys"
