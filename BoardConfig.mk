aab58f600a3c04bc553b67ced2148c67f3eca073
drgroovestarr <jarrod.worlitz@gmail.com>
2016-01-07 02:05:08 -0800

    Update BoardConfig.mk For Kylo kernel by DespairFactor Also update kernel GCC to 5.3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,10 +32,10 @@ TARGET_2ND_CPU_VARIANT := cortex-a7
 ENABLE_CPUSETS := true
 
 # Inline kernel building
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9-kernel/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-5.3-kernel/bin
 KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/huawei/angler
-TARGET_KERNEL_CONFIG := angler_defconfig
+TARGET_KERNEL_CONFIG := kylo_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
 BOARD_KERNEL_BASE        := 0x00000000
