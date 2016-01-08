929f922cb5a4414e40329cbaf3f6f8a3a8aced06
mydongistiny <jaysonedson@gmail.com>
2016-01-07 19:47:29 -0800

    angler: Add in missing v4a props

--- device.mk
+++ device.mk
@@ -169,6 +169,13 @@ PRODUCT_PACKAGES += \
     libOmxVdecHevc \
     libOmxVenc
 
+# Viper4Android
+PRODUCT_PROPERTY_OVERRIDES += \
+    lpa.decode=false \
+    lpa.releaselock=false \
+    lpa.use-stagefright=false \
+    tunnel.decode=false
+
 #CAMERA
 PRODUCT_PACKAGES += \
     camera.msm8994 \
