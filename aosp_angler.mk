86afa08125f92917d19f4ab1457fdfe8ecdff601
Jarrod Worlitz <jarrod.worlitz@gmail.com>
2016-01-11 10:18:25 +0000

    Dont need Launcher3

--- aosp_angler.mk
+++ aosp_angler.mk
@@ -33,7 +33,3 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 $(call inherit-product, device/huawei/angler/device.mk)
 $(call inherit-product-if-exists, vendor/huawei/angler/device-vendor.mk)
-
-PRODUCT_PACKAGES += \
-    Launcher3
-
