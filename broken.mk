3f99aa7fae548f90d91433a16e45f542020da991
drgroovestarr <jarrod.worlitz@gmail.com>
2016-05-28 13:42:18 -0700

    Broke-it: Add broken.mk and broken.dependencies

---
+++ broken.mk
@@ -0,0 +1,25 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+# Inherit Broken common.
+$(call inherit-product, vendor/broken/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/broken/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/huawei/angler/aosp_angler.mk)
+$(call inherit-product, vendor/huawei/angler/device-vendor.mk)
+DEVICE_PACKAGE_OVERLAYS += device/huawei/angler/overlay-cm
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := broken_angler
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 6P
+
+TARGET_VENDOR := Huawei
+
+# Broken Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	DEVICE_MAINTAINERS="Jarrod Worlitz (drgroovestarr)"
